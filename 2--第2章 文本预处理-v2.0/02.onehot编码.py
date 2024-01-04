d41451270cd59101b686170890e41958ad44e738
Shimon-Cheung <25337942@qq.com>
2024-01-04 23:58:32 +0800

    [add]完成onehot编码以及，使用fasttext库快速训练一个词向量模型

---
+++ 2--第2章 文本预处理-v2.0/02.onehot编码.py
@@ -0,0 +1,39 @@
+﻿# 导入用于对象保存与加载的joblib
+import joblib
+# 导入keras中的词汇映射器Tokenizer
+from keras.preprocessing.text import Tokenizer
+
+# 假定vocab为语料集所有不同词汇集合
+vocab = {"周杰伦", "陈奕迅", "王力宏", "李宗盛", "吴亦凡", "鹿晗"}
+# 实例化一个词汇映射器对象
+t = Tokenizer(num_words=None, char_level=False)
+# 使用映射器拟合现有文本数据
+t.fit_on_texts(vocab)
+
+for token in vocab:
+    zero_list = [0] * len(vocab)
+    # 使用映射器转化现有文本数据, 每个词汇对应从1开始的自然数
+    # 返回样式如: [[2]], 取出其中的数字需要使用[0][0]
+    token_index = t.texts_to_sequences([token])[0][0] - 1
+    zero_list[token_index] = 1
+    print(token, "的one-hot编码为:", zero_list)
+
+# 使用joblib工具保存映射器, 以便之后使用
+tokenizer_path = "./Tokenizer"
+print(joblib.dump(t, tokenizer_path))
+
+# 比如我有一个此表，里面有3w个词汇。那么每次表示一个词那么都是3w元素的一个列表，然后对应的词汇哪个索引是1别的都是0.这样就可以表示这个词的位置了。
+# 这个的好处就是把词用一个向量表示了方便计算进行科学计算。
+# 缺点就是每个向量的长度过大，占据大量内存.比如一共10w词，那么表示每一个词都是10w的长度的列表
+
+# 加载之前保存的Tokenizer, 实例化一个t对象
+t = joblib.load(tokenizer_path)
+# 编码token为"李宗盛"
+token = "李宗盛"
+# 使用t获得token_index
+token_index = t.texts_to_sequences([token])[0][0] - 1
+# 初始化一个zero_list
+zero_list = [0] * len(vocab)
+# 令zero_List的对应索引为1
+zero_list[token_index] = 1
+print(token, "的one-hot编码为:", zero_list)
---
+++ 2--第2章 文本预处理-v2.0/03.word2vec词向量.py
@@ -0,0 +1,19 @@
+﻿# 词汇表示成向量的无监督训练方法, 该过程将构建神经网络模型, 将网络参数作为词汇的向量表示, 它包含CBOW和skipgram两种训练模式
+# CBOW(Continuous bag of words)模式
+# 代码运行在python解释器中
+# 导入fasttext
+import fasttext
+
+# 使用fasttext的train_unsupervised(无监督训练方法)进行词向量的训练
+# 它的参数是数据集的持久化文件路径'data/fil9'
+# model = fasttext.train_unsupervised('./fil9', "skipgram", dim=300, epoch=1, lr=0.1, thread=8)
+# 保存训练之后的模型
+# model.save_model("fil9.bin")
+
+
+# 使用fasttext.load_model加载模型
+model = fasttext.load_model("fil9.bin")
+print(model.get_word_vector("the"))
+
+# 查找和音乐相近的词
+print(model.get_nearest_neighbors('music'))
